a4245af8a7122a510bfc9975e0c4658c0abfd7cb
lilong <15922896158@163.com>
2022-07-23 20:01:36 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { computed, onMounted, onUnmounted } from 'vue'
+import { zhCN, dateZhCN, darkTheme, useLoadingBar } from 'naive-ui'
+import { useMainStore } from './store/index'
+
+const mainStore = useMainStore()
+const getDarkTheme = computed(() => (mainStore.isDark ? darkTheme : undefined))
+const isLock = computed(() => mainStore.isLock)
+
+</script>
+
+<template>
+  <NConfigProvider
+    v-if="!isLock"
+    :locale="zhCN"
+    :theme="getDarkTheme"
+    :date-locale="dateZhCN"
+  >
+    <router-view />
+  </NConfigProvider>
+</template>
+
+<style></style>
